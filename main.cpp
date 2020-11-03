3806bdca20309fc3322ac6acb81938edbb894c8f
pescoboza <psebastian01@hotmail.com>
2020-11-02 19:58:27 -0700

    :white_check_mark: Added string test.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include "Graph.hpp"
 
+#include <string>
 
 
-int main() {
+
+void test1() {
+	auto printValue{ 
+		[](const int& v){
+			std::cout << v << " -> ";
+		}
+	};
+	
 	Graph<int> graph;
 
-	graph.addNodeCpy(1, {0}, {2,3})
-		.addNodeCpy(3, {}, {1});
+	//              node,	parents, children
+	graph.addNodeCpy(1,		{0},	 {2,3})
+		 .addNodeCpy(3,		{},		 {1});
 
-
-	auto printValue{ 
-		[](const int& v) {
-			std::cout << v << " -> ";
-		} 
-	};
 
 	std::cout << "=== ADJ LIST ===\n\n";
 	graph.print();
@@ -23,8 +26,42 @@ int main() {
 	
 	std::cout << "\n\n=== BFS ===\n\n";
 	graph.breathFirstSearch(0, printValue);
-	
-	
-	
+		
+	std::cout << "\n\n";
+}
+
+void test2() {
+	auto printValue{ 
+		[](const std::string& v) {
+			std::cout << v << " -> ";
+		}
+	};
+
+
+	Graph<std::string> graph;
+
+	graph.addNodeCpy("hello", { "I", "live" }, { "here" })
+ 		.addNodeCpy("I", { "hello" }, { "here" });
+
+	std::cout << "=== ADJ LIST ===\n\n";
+	graph.print();
+
+	std::cout << "\n\n=== DFS ===\n\n";
+	graph.depthFirstSearch("hello", printValue);
+
+	std::cout << "\n\n=== BFS ===\n\n";
+	graph.breathFirstSearch("hello", printValue);
+
+	std::cout << "\n\n";
+}
+
+int main() {
+	std::cout << "======= Test 1 =======\n";
+	test1();
+
+	std::cout << "======= Test 2 =======\n";
+	test2();
+
+
 	return 0;
 }
