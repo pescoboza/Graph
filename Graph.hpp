95fb621e698849a1cfb193403b96dca7f7bac720
pescoboza <psebastian01@hotmail.com>
2020-11-02 18:07:02 -0700

    :splarkles: Added and tested DFS.

--- Graph.hpp
+++ Graph.hpp
@@ -16,9 +16,10 @@ public:
 	Graph& addNodeRef(const T& value, const std::vector<T>& parents, const std::vector<T>& children);
 	Graph& addNodeCpy(const T& value, std::vector<T> parents, std::vector<T> children);
 	template <class UnaryFunction>
-	Graph& breathFirstSearch(const T& head, UnaryFunction f) const;
+	Graph& breathFirstSearch(const T& head, UnaryFunction f);
 	template <class UnaryFunction>
-	Graph& depthFirstSearch(const T& head, UnaryFunction f) const;
+	Graph& depthFirstSearch(const T& head, UnaryFunction f);
+
 
 	void print(std::ostream& out = std::cout)const {
 		for (const auto& nodePair : m_table) {
@@ -30,7 +31,11 @@ public:
 		}
 	}
 
-
+private:
+	template <class UnaryFunction>
+	void bfs(const T& value, UnaryFunction f, Children& visited);
+	template <class UnaryFunction>
+	void dfs(const T& value, UnaryFunction f, Children& visited);
 };
 
 
@@ -77,3 +82,36 @@ inline Graph<T>& Graph<T>::addNodeCpy(const T& value, std::vector<T> parents, st
 
 #endif // !GRAPH_HPP
 
+template<typename T>
+template<class UnaryFunction>
+inline Graph<T>& Graph<T>::depthFirstSearch(const T& head, UnaryFunction f){
+	Children visited;
+	dfs(head, f, visited);
+	return *this;
+}
+
+template<typename T>
+template<class UnaryFunction>
+inline void Graph<T>::dfs(const T& value, UnaryFunction f, Children& visited){
+	// Look for value in node list
+	auto it{m_table.find(value)};
+	if (it == m_table.end())
+		return;
+
+	{
+		// Visit the node
+		const T& cref{ it->first };
+		f(cref);
+	}
+	
+	// Remember node as visited
+	visited.emplace(&it->first);
+	
+	// Iterate through children
+	for (const auto& childPtr : it->second) {
+		
+		// If new node is inserted, it must be visited
+		if (visited.emplace(childPtr).second)
+			dfs(*childPtr, f, visited); // Pass olympic torch
+	}
+}
--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main() {
 	graph.addNodeCpy(6, { 5 }, {7,8})
 		.addNodeCpy(5, { 6, 4, 1, 2 }, {7,10});
 
+	graph.depthFirstSearch(6, [](const int& value) {std::cout << value << " -> "; });
+	std::cout << '\n';
+
 	graph.print();
 	return 0;
 }
