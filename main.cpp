c4fb02373b2614aecbfb90a6d3d349443296649c
pescoboza <psebastian01@hotmail.com>
2020-11-03 20:46:38 -0700

    :white_check_mark: Added shear tests.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 
 
 
+// Basic functionality test with simplest insertion method:
+// Insert nodes with family star: node, parents, and children.
+// Demonstration of BFS and DFS.
+// Printing of adjacency list.
 void test1() {
 	Graph<int> graph;
 
@@ -25,6 +29,7 @@ void test1() {
 	std::cout << "\n\n";
 }
 
+// Test for complex data types.
 void test2() {
 
 	Graph<std::string> graph;
@@ -46,12 +51,9 @@ void test2() {
 	std::cout << "\n\n";
 }
 
+// Test for template specialization for unsigned int with 
+// construction from matrix of adjacency (exclusive for unsigned). 
 void test3() {
-	auto printValue{
-		[](const unsigned& v) {
-			std::cout << v << " -> ";
-		}
-	};
 
 	std::vector<std::vector<bool>> adjMat{
 		// 0  1  2  3  4
@@ -78,10 +80,103 @@ void test3() {
 
 }
 
-void test4() {
 
+// Testing construction from adjacency list.
+void test4() {
+	std::vector<std::pair<unsigned, std::vector<unsigned>>> adjList{
+		{0,{ 1, 2 }},
+		{1,{ 3, 4 }},
+		{2,{ 3    }},
+		{3,{      }},
+		{4,{ 5    }},
+		{5,{ 1, 3 }}
+	};
+
+	GraphUint graph{ adjList };
+	auto root{ 0U };
+
+	std::cout << "=== Adjacency List ===\n\n";
+	graph.printAdjList();
+
+	std::cout << "\n\n=== DFS ===\n\n";
+	graph.DFS(root);
+
+	std::cout << "\n\n=== BFS ===\n\n";
+	graph.BFS(root);
+
+	std::cout << "\n\n";
 }
 
+// Shear test.
+void test5() {
+	
+	const unsigned NUM_NODES{ 1000 };
+	const unsigned MAX_NUM_CHILDREN{ 10 };
+	
+	
+	std::vector<std::pair<unsigned, std::vector<unsigned>>> adjList;
+	adjList.reserve(NUM_NODES);
+	for (unsigned i{ 0U }; i < NUM_NODES; ++i) {
+		
+		adjList.emplace_back(i, std::vector<unsigned>{});
+		auto& children{adjList.back().second};
+		
+		unsigned numChildren{ std::rand() % MAX_NUM_CHILDREN };
+		for (unsigned chNum{ 0U }; chNum < numChildren; ++chNum)
+			children.emplace_back(std::rand() % NUM_NODES);
+
+	}
+
+	GraphUint graph{ adjList };
+	auto root{ 0U };
+
+	std::cout << "=== Adjacency List ===\n\n";
+	graph.printAdjList();
+
+	std::cout << "\n\n=== DFS ===\n\n";
+	graph.DFS(root);
+
+	std::cout << "\n\n=== BFS ===\n\n";
+	graph.BFS(root);
+
+	std::cout << "\n\n";
+}
+
+
+// SUPER Shear test.
+void test6() {
+
+	const unsigned NUM_NODES{ 10000 };
+	const unsigned MAX_NUM_CHILDREN{ 100 };
+
+
+	std::vector<std::pair<unsigned, std::vector<unsigned>>> adjList;
+	adjList.reserve(NUM_NODES);
+	for (unsigned i{ 0U }; i < NUM_NODES; ++i) {
+
+		adjList.emplace_back(i, std::vector<unsigned>{});
+		auto& children{ adjList.back().second };
+
+		unsigned numChildren{ std::rand() % MAX_NUM_CHILDREN };
+		for (unsigned chNum{ 0U }; chNum < numChildren; ++chNum)
+			children.emplace_back(std::rand() % NUM_NODES);
+
+	}
+
+	GraphUint graph{ adjList };
+	auto root{ 0U };
+
+	std::cout << "=== Adjacency List ===\n\n";
+	graph.printAdjList();
+
+	std::cout << "\n\n=== DFS ===\n\n";
+	graph.DFS(root);
+
+	std::cout << "\n\n=== BFS ===\n\n";
+	graph.BFS(root);
+
+	std::cout << "\n\n";
+}
 
 int main() {
 	std::cout << "======= Test 1 =======\n";
@@ -93,6 +188,20 @@ int main() {
 	std::cout << "======= Test 3 =======\n";
 	test3();
 
+	std::cout << "======= Test 4 =======\n";
+	test4();
+
+	std::cout << "Press ENTER to continue....";
+	std::cin.get();
+
+	std::cout << "======= Test 5 =======\n";
+	test5();
+
+	std::cout << "======= Test 6 =======\n";
+	test6();
+
+	std::cout << "Press ENTER to exit.";
+	std::cin.get();
 
 	return 0;
 }
